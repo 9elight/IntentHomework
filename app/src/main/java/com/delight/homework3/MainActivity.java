b343b3481346fabb4ca4b1f0e5ec31d1ad79716b
User <moriartir@gmail.com>
2019-10-05 18:09:09 +0600

    1.1

---
+++ app/src/main/java/com/delight/homework3/MainActivity.java
@@ -0,0 +1,38 @@
+package com.delight.homework3;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.calculatorButton);
+
+    }
+
+    public void openCalculator(View v){
+        Intent intent = new Intent(this,CalculatorActivity.class);
+        startActivityForResult(intent,2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2){
+            if (resultCode == RESULT_OK){
+                TextView textView = findViewById(R.id.calculateResult);
+                textView.setText(data.getStringExtra("calculateResult"));
+            }
+        }
+    }
+}
